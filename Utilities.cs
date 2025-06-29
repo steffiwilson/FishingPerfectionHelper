66a2727620ec7f0ffb3108adfc9c00977e2ef09a
Stephanie Wilson <swilson@masters.edu>
2025-06-29 16:07:46 -0700

    Learned how to do line breaks correctly

--- Utilities.cs
+++ Utilities.cs
@@ -10,11 +10,9 @@ namespace FishingPerfectionHelper
     {
         public static string BuildCatchableFishListForDisplay(List<Fish> catchableFish)
         {
-            string message = //line breaks as they display in the 52?-character monospace window in-game
-            "These are the fish that you haven't caught yet     " +
-            "that you should be able to catch under the current " +
-            "season, conditions, and time:                      " +
-            "                                                     ";
+            string message = 
+            "These are the fish that you haven't caught yet that you should be able to catch under the " +
+            "current season, conditions, and time:^^";
 
             int lineCount = 4;
             catchableFish = catchableFish.OrderBy(f => f.Difficulty).ToList();
@@ -26,25 +24,25 @@ namespace FishingPerfectionHelper
                 switch (lineCount)
                 {
                     case 11:
-                        message += "Page 2...                                      ";
+                        message += "Page 2...^";
                         break;
                     case 22:
-                        message += "Page 3...                                     ";
+                        message += "Page 3...^";
                         break;
                     case 33:
-                        message += "Page 4...                                       ";
+                        message += "Page 4...^";
                         break;
                     case 44:
-                        message += "Page 5...                                        ";
+                        message += "Page 5...^";
                         break;
                     case 55:
-                        message += "Page 6...                                         ";
+                        message += "Page 6...^";
                         break;
                     case 66:
-                        message += "Page 7...                                      ";
+                        message += "Page 7...^";
                         break;
                     case 77: //this is enough for all fish in 1.6
-                        message += "Page 8...                                       ";
+                        message += "Page 8...^ ";
                         break;
                     default: //not a first line on a page, so just build a fish info line
                         //limit what we display bc of the ~50 char limit which runs out easily
@@ -54,18 +52,15 @@ namespace FishingPerfectionHelper
                             rain = " - rain";
                         }
 
-                        string thisFish = ($"> {fish.Name} ({fish.Locations}){rain}");
+                        string thisFish = ($"> {fish.Name} ({fish.Locations}){rain}^");
                         // eg '> Pufferfish (Ocean)' or '> Walleye (Freshwater) - rain'
 
                         //truncate (sorry) if somehow too long 
-                        if (thisFish.Length > 52)
-                            thisFish = thisFish.Substring(0, 50);
-
-                        while (thisFish.Length < 52)
+                        if (thisFish.Length > 50)
                         {
-                            thisFish += " "; //append spaces until it fills the line lol what could possibly go wrong
+                            thisFish = thisFish.Substring(0, 49);
+                            thisFish += "^";
                         }
-
                         message += thisFish;
                         break;
                 } //end switch
@@ -73,7 +68,6 @@ namespace FishingPerfectionHelper
             } //end foreach
 
             return message;
-
         }
         public static List<int> GetTimeRange(int start, int end)
         {
